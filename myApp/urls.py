dbbe78ee0be0835bcb6e3af52781332384b51b59
icarus <ayushask.2002@gmail.com>
2024-07-11 11:48:12 +0545

    Added user profile

--- myApp/urls.py
+++ myApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import contact, register ,login
+from .views import contact, register ,login,user_profile_view
 
 urlpatterns = [ 
 path('register/',register),    
 path('login/', login),
-path('contact/',contact)
+path('contact/',contact),
+path('user-profile',user_profile_view),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -7,6 +7,7 @@ from .models import CustomUser
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import AccessToken
 from myApp.authentication import UserAuthentication
 @api_view(['POST'])
 def register(request):
@@ -36,6 +37,8 @@ def login(request):
     
     refresh = RefreshToken.for_user(user)
     return Response({
+        "user":user.username,
+        "role":user.role,
         "message": "Login successful",
         "token": str(refresh.access_token),
     }, status=status.HTTP_200_OK) 
@@ -51,3 +54,23 @@ def contact(request):
         return Response({"message":str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     return Response({"Message":"Data Saved"},status=status.HTTP_201_CREATED)
     
+
+
+
+@api_view(['GET','PUT'])
+@authentication_classes([UserAuthentication])
+def user_profile_view(request):
+    try:
+        user=request.user
+    except CustomUser.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method=="GET":
+        serializer = CustomUserSerializer(user)
+        return Response(serializer.data)
+    elif request.method=="PUT":
+        serializer= CustomUserSerializer(user,data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
